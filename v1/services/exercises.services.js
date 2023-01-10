c6e9f81980f584c51529d9510b8b0343262deb29
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-10 16:27:51 +0600

    queries and filters

--- v1/services/exercises.services.js
+++ v1/services/exercises.services.js
@@ -1,15 +1,20 @@
-const Exercises = require('../models/Exercises');
+const Exercises = require("../models/Exercises");
 
-exports.getAllExerciseService = async () => {
-    const data = await Exercises.find({});
-    const totalExercises = await Exercises.countDocuments({});
+exports.getAllExerciseService = async (filters, queries) => {
+  const data = await Exercises.find(filters)
+    .skip(queries.skip)
+    .limit(queries.limit)
+    .select(queries.fields)
+    .sort(queries.sortBy);
 
-    return {totalExercises, data};
-}
+  const totalExercises = await Exercises.countDocuments(filters);
+  const pageCount = Math.ceil(totalExercises / queries.limit);
+
+  return { totalExercises,pageCount , data };
+};
 
 exports.createExerciseService = async (data) => {
+  const result = await Exercises.create(data);
 
-    const result = await Exercises.create(data);
-
-    return result;
-}
+  return result;
+};
